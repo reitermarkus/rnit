731b30a8262ef5a254636dd25cabe792e3d69fd8
Markus Reiter <me@reitermark.us>
2017-05-14 01:35:10 +0200

    Add script to set up Apache server.

---
+++ uebung7/setup_apache.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+apt-get update
+apt-get upgrade
+apt-get install apache2
+service apache2 start
+
+usermod -a -G www-data student
+
+chgrp -R www-data /var/www
+chmod -R g+wr /var/www
+
+cat <<EOF > /etc/apache2/sites-available/public.conf
+<VirtualHost *:80>
+  ServerAdmin webmaster@localhost
+  DocumentRoot /var/www/public
+  ErrorLog ${APACHE_LOG_DIR}/error.log
+  CustomLog ${APACHE_LOG_DIR}/access.log combined
+</VirtualHost>
+EOF
+
+cat <<EOF > /etc/apache2/sites-available/secret.conf
+Listen 4567
+
+<VirtualHost *:4567>
+  ServerAdmin webmaster@localhost
+  DocumentRoot /var/www/secret
+  ErrorLog ${APACHE_LOG_DIR}/error.log
+  CustomLog ${APACHE_LOG_DIR}/access.log combined
+</VirtualHost>
+EOF
+
+rm -f /etc/apache2/sites-enabled/000-default.conf
+ln -sfn /etc/apache2/sites-enabled/public.conf /etc/apache2/sites-available/public.conf
+ln -sfn /etc/apache2/sites-enabled/secret.conf /etc/apache2/sites-available/secret.conf
+
+mkdir -p /var/www/public
+mkdir -p /var/www/secret
+
+cat <<EOF > /var/www/public/index.html
+<!DOCTYPE html>
+<html>
+  <body>
+    <pre>Diese Seite tut nichts Verbotenes.</pre>
+  </body>
+</html>
+EOF
