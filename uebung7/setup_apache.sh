ff966373f9e9b0ce63c9723fcf0d522bfe8ac02b
Michael Kaltschmid <kaltschmidmichael@gmail.com>
2017-05-15 02:17:44 +0200

    Add fake hosts.

--- uebung7/setup_apache.sh
+++ uebung7/setup_apache.sh
@@ -19,6 +19,21 @@ cat <<EOF > /etc/apache2/sites-available/public.conf
 </VirtualHost>
 EOF
 
+cat <<EOF > /etc/apache2/sites-available/microsoftfake134.conf
+<VirtualHost *:80>
+    ServerName microsoftfake134.com
+    DocumentRoot /var/www/microsoftfake134
+</VirtualHost>
+EOF
+
+
+cat <<EOF > /etc/apache2/sites-available/githubfake134.conf
+<VirtualHost *:80>
+    ServerName githubfake134.com
+    DocumentRoot /var/www/githubfake134
+</VirtualHost>
+EOF
+
 cat <<EOF > /etc/apache2/sites-available/secret.conf
 Listen 4567
 
@@ -37,11 +52,32 @@ ln -sfn /etc/apache2/sites-enabled/secret.conf /etc/apache2/sites-available/secr
 mkdir -p /var/www/public
 mkdir -p /var/www/secret
 
+mkdir -p /var/www/githubfake134
+mkdir -p /var/www/microsoftfake134
+
 cat <<EOF > /var/www/public/index.html
 <!DOCTYPE html>
 <html>
   <body>
     <pre>Diese Seite tut nichts Verbotenes.</pre>
+  </body>
+</html>
+EOF
+
+cat <<EOF > /var/www/microsoftfake134/index.html
+<!DOCTYPE html>
+<html>
+  <body>
+    <pre>fakehost[1,2] tut nichts Verbotenes.</pre>
+  </body>
+</html>
+EOF
+
+cat <<EOF > /var/www/githubfake134/index.html
+<!DOCTYPE html>
+<html>
+  <body>
+    <pre>fakehost[2,2] tut nichts Verbotenes.</pre>
   </body>
 </html>
 EOF
